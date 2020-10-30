e587f3e95c1aa6b6ce36b1044b6135129a2e36b9
Ge (Melody) Zhang <59809428+MelodyZhangHelloWorld@users.noreply.github.com>
2020-10-30 02:04:17 -0600

    simpleQuery_googleBookAPI

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This page is for Book Shelf</h1>
+    <h1>This page is for </h1>
+    <h2>
+      <Shelf msg2="Book Shelf"/>
+    
+    </h2>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import Shelf from '@/components/Shelf.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Shelf
+  }
+}
+</script>
